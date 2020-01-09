390e378069b2797b7cae9cbe847c214fb9af59f8
Richard Bischof <richard.bischof@lgln.niedersachsen.de>
2020-01-09 18:49:50 +0100

    fixed gateway url

--- scripts/requests-android.sh
+++ scripts/requests-android.sh
@@ -3,7 +3,7 @@
 while true; do
 	counter=$((counter+1))
 	echo "$counter: Request to cluster as android user"
-	curl http://34.67.217.149/productpage -o /dev/null -s -A "Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
+	curl http://$(echo GATEWAY_URL)/productpage -o /dev/null -s -A "Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
 	sleep .5
 
 done	
--- scripts/requests-ios.sh
+++ scripts/requests-ios.sh
@@ -3,7 +3,7 @@
 while true; do
 	counter=$((counter+1))
 	echo "$counter: Request to cluster as iOS user"
-	curl http://34.67.217.149/productpage -o /dev/null -s -A "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
+	curl http://$(echo GATEWAY_URL)/productpage -o /dev/null -s -A "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
 	sleep .5
 
 done	
