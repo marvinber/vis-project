bedc7fb03c24bbc2e18cbf4475bcbc7c24f1e847
marvinber <marvin.bertram@stud.hs-hannover.de>
2020-01-15 19:35:22 +0100

    simple changes to scripts

--- scripts/requests-android.sh
+++ scripts/requests-android.sh
@@ -5,7 +5,6 @@ while true; do
 	echo "$counter: Request to cluster as android user"
 	curl http://$(echo GATEWAY_URL)/productpage -o /dev/null -s -A "Mozilla/5.0 (Linux; U; Android 2.2.1; en-us; Nexus One Build/FRG83) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
 	sleep .5
-
 done	
 
 	
--- scripts/requests-ios.sh
+++ scripts/requests-ios.sh
@@ -5,7 +5,6 @@ while true; do
 	echo "$counter: Request to cluster as iOS user"
 	curl http://$(echo GATEWAY_URL)/productpage -o /dev/null -s -A "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
 	sleep .5
-
 done	
 
 	
